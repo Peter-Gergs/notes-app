d69d79a9a6922dcf80b018cbcf40f0f52bdeeae2
Peter-Gergs <80621754+Peter-Gergs@users.noreply.github.com>
2023-08-09 14:36:47 +0300

    fix router

--- src/App.js
+++ src/App.js
@@ -17,12 +17,15 @@ function App() {
     <main id="app">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Notes notes={notes} />} />
+          <Route path="/notes-app" element={<Notes notes={notes} />} />
           <Route
-            path="/create-note"
+            path="/notes-app/create-note"
             element={<CreateNote setNotes={setNotes} />}
           />
-          <Route path="/edit-note/:id" element={<EditNote notes={notes} setNotes={setNotes} />} />
+          <Route
+            path="/notes-app/edit-note/:id"
+            element={<EditNote notes={notes} setNotes={setNotes} />}
+          />
         </Routes>
       </BrowserRouter>
     </main>
